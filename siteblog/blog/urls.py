c6812d04fcbd94154c793bfbd6b90bf182205a63
akademik <pvv2606@gmail.com>
2020-11-15 15:09:33 +0200

    I have implemented dysplay articles by categories

--- siteblog/blog/urls.py
+++ siteblog/blog/urls.py
@@ -4,7 +4,8 @@ from .views import *
 urlpatterns = [
 	# path('', index, name='home'),
 	path('', Home.as_view(), name='home'),
-	path('category/<str:slug>/', get_category, name='category'),
+	# path('category/<str:slug>/', get_category, name='category'),
+	path('category/<str:slug>/', PostsByCategory.as_view(), name='category'),
 	path('post/<str:slug>/', get_post, name='post'),
 
 
