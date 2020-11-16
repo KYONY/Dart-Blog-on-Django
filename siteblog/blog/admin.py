74e463e6094d50cafb736863aa7fbfa0000e13d6
akademik <pvv2606@gmail.com>
2020-11-16 15:52:24 +0200

    I have implemented dysplay sidebar and display popular posts

--- siteblog/blog/admin.py
+++ siteblog/blog/admin.py
@@ -20,10 +20,10 @@ class PostsAdmin(admin.ModelAdmin):
 	form = PostAdminForm
 	save_as = True
 	save_on_top = True
-	list_display = ('id', 'title', 'slug', 'category', 'created_at', 'get_photo')
+	list_display = ('id', 'title', 'slug', 'category', 'created_at', 'get_photo','views')
 	list_display_links = ('id','title')
 	search_fields = ('title',)
-	list_filter = ('category', 'tags')
+	list_filter = ('category', 'tags', 'views')
 	readonly_fields = ('views','created_at', 'get_photo')
 	fields = ('title', 'slug', 'category', 'tags', 'content','photo','get_photo','views', 'created_at')
 
